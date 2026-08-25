cac2a18a45b1d6bd5be2b98bb326931540237054
Suhaim Al Yafei <suhaim.qatar09@gmail.com>
2026-08-25 09:47:52 +0300

    Fix CORS for local and Firebase frontend

--- demo_api.py
+++ demo_api.py
@@ -29,6 +29,8 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "https://c-arm-guidance-simulator.web.app",
+        "https://c-arm-guidance-simulator.firebaseapp.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
