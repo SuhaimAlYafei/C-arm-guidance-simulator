771b19dc168d5ce14c3ab9a0c5b6357b35dfffe2
Suhaim AlYafei <suhaim.qatar09@gmail.com>
2026-08-28 06:14:48 +0300

    fix(v3): unlock transform controls when OR equipment is available

--- 3DVisualizer/ciartic-app/src/SimulatorShell.jsx
+++ 3DVisualizer/ciartic-app/src/SimulatorShell.jsx
@@ -5,6 +5,7 @@ import ResearchControlDock from './components/ResearchControlDock.jsx';
 import AwardStudyPanel from './components/AwardStudyPanel.jsx';
 import './scene/orDragOverride.js';
 import './scene/realisticOperatingRoomAssets.js';
+import './scene/orTransformUiCompatibility.js';
 
 const parseNumber = (value) => {
   const parsed = Number(value);
